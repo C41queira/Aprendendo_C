2319ca927c670a0235d84b8f3502015a4007f8df
Caick Bertin <bertinviana@gmail.com>
2023-03-23 09:27:41 -0300

    Primeiros passos na linguagem C

---
+++ PrimeiroPrograma.c
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+
+//define uma informação de maneiraa definitiva
+#define PI 3.1415
+
+//Main do programa, escopo onde todo o programa será realizado
+int main(){
+    //Metodo de saida, podendo abrangir Strings, Ints, Floats, Chars,  
+    printf("Hello word\n");
+
+    //para se criar um texto tem que se criar um vetor de char
+    const char nome [] = "Caick"; 
+    int idade = 20; 
+
+    double altura = 1.84; 
+    double peso = 87.0; 
+    double imc = peso/(altura*altura);  
+
+
+    printf("Meu nome é %s e tenho %d anos\n", nome, idade);
+    printf("Eu tenho %.2f de altura e peso %.2f e meu IMC é %.2f", altura, peso, imc); 
+
+    //%.2f numero com duas casa depois da virgula 
+    //%2.f numero com duas casa antes da virgula 
+    printf("Valor de PI: %.4f\n", PI); 
+
+    //transforma uma variavel em algo constante
+    const int gravidade = 10;
+    printf("Valor da gravidade: %d\n", gravidade);
+
+    //Aceita apenas texto 
+    pust("Essa aula não acaba");
+
+    //Aceita textos e veriaveis
+    printf("Aceito texto e varia"); 
+    
+    //Aceita apenas a saida de um caracter
+    putchar('a');
+
+
+
+    return 0; 
+}
---
+++ Variaveis_Em_C.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    //Instanciações de Variaveis:
+
+    //short - numeros inteiros
+    //16 bits
+    short numerosCurtos = 45; 
+
+    //int - numeros inteiros(1, 3, -8, 103)
+    // 16 a 32 bits
+    int numeroInteiro = 8; 
+
+    //long - numeros inteiros 
+    //32 a 64 bits
+    long numerosLongos = 555555555555555; 
+
+    //longlong - numeros inteiros 
+    //64 bits
+    long numerosLongosLongos = 555555555555555; 
+
+
+    //char - caracteres simples('a', 'B', '$')
+    //8 bits
+    char caractere = '%';
+
+
+    //float - ponto flutuante ou numeros com virgulas com uma casa decimal(1.5, 6.8, 7.9)
+    //32 bits
+    float flutuante = 5.6; 
+
+    //double - ponto flutuante com double-precision ou  numeros com virgulas com mais de uma casa decimal(1.5567, 2.645, -4.777)
+    //64 bits
+    double flutuantes = 4.668; 
+
+
+    //Void não traz retorno
+}
